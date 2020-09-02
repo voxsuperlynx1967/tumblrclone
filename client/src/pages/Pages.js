d48350eec7cf1ecc1089650af3ae6b3b17feaaba
Simon Carroll <snc40@georgetown.edu>
2020-09-02 19:46:08 -0400

    navbar

--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -6,11 +6,16 @@ import LoginPage from './LoginPage';
 
 import SignupPage from './SignupPage';
 
+import Dashboard from './Dashboard';
+
 export default function Pages() {
   return (
     <>
       <Route path="/login" component={LoginPage} />
       <Route path="/signup" component={SignupPage} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/dashboard" component={Dashboard} />
+
       <Route exact path="/">
         <h1>My Home Page</h1>
       </Route>
