a3c8efb9d02332314028de35562383452d8b1af2
Simon Carroll <snc40@georgetown.edu>
2020-09-02 16:46:53 -0400

    reverted change on login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -42,9 +42,6 @@ function LoginPage() {
         fixed
         maxWidth="sm">
         <YumblrLogo/>
-        <h1>Come for what you crave.
-            Stay for what you eat.
-        </h1>
         <form onSubmit={handleSubmit}>
           <AuthTextField id="textfield1"
             placeholder="Email"
