e8a3cb5cb372365f4e250ad74c6ed366334f4b20
Simon Carroll <snc40@georgetown.edu>
2020-10-12 18:39:47 -0400

    database issue

---
+++ db/seeders/20201012223002-users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+function createPassword() {
+  return bcrypt.hashSync('password');
+}
+
+function r(o) {
+  o.createdAt = new Date();
+  o.updatedAt = new Date();
+  return o;
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      r({ username: 'guyfieri', email: 'flavor@example.com', hashedPassword: createPassword() }),
+      r({ username: 'chefgordon', email: 'grams@example.com', hashedPassword: createPassword() }),
+      r({ username: 'fishflay', email: 'bflay@example.com', hashedPassword: createPassword() }),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users');
+  }
+};
